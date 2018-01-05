2764d1639d254e18dd240444bedeacb8b34c9e82
Kimmo Koskinen <kimmo@tiuhti.net>
2018-01-06 00:05:38 +0200

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject viesti/table-spec "0.1.1"
+(defproject viesti/table-spec "0.1.2-SNAPSHOT"
   :description "clojure.spec from SQL schema"
   :url "http://github.com/viesti/table-spec"
   :license {:name "Eclipse Public License"
